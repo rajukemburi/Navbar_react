90a7b23852b1fdbab283f018d1c5719b8452dcbd
rajukemburi <rajukemburi2004@gmail.com>
2025-10-25 20:05:23 +0530

    navbar added

---
+++ Navbar_react/src/App.jsx
@@ -0,0 +1,31 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+  
+
+
+    
+  <div class="navbar">
+    
+    <nav id='nav'>
+      <ul> 
+      <li><h1 style={{paddingRight:"90px",paddingTop:"20px"  }}>Navbar</h1></li>
+      <li><a href="#home"><h3>Home</h3></a></li>
+      <li><a href="#about"><h3>about</h3></a></li>
+      <li><a href="#services"><h3>Contact</h3></a></li>
+      <li><a href="#contact"><h3>Services</h3></a></li>
+      </ul>
+      {/* <div class="btn">
+         <a href="#btn"><button>Login</button></a>
+         <a href="btn"><button>Signup</button></a>
+      </div> */}
+      
+    </nav>
+  </div>
+  )
+}
+
+export default App
